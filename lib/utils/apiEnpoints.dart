b0b406508f123416ec9ee3679937c3a1398fff14
vunghiak3 <vunghiak3@gmail.com>
2025-04-07 03:43:08 +0700

    refactor: change baseUrl in apiEndpoints

--- lib/utils/apiEnpoints.dart
+++ lib/utils/apiEnpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static final String baseUrl = 'http://10.0.2.2:8000';
+  static final String baseUrl = 'http://localhost:8000';
   static final _AuthEndpoints auth = _AuthEndpoints();
   static final _Userenpoints user = _Userenpoints();
 }
